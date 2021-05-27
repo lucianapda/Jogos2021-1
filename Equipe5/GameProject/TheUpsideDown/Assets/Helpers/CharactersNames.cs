276a3d415068dc2527ec4f585a8afd8c63ffe819
Caroline <33700047+cregalin@users.noreply.github.com>
2021-05-26 21:49:27 -0300

    Campo nome do usuário

--- Equipe5/GameProject/TheUpsideDown/Assets/Helpers/CharactersNames.cs
+++ Equipe5/GameProject/TheUpsideDown/Assets/Helpers/CharactersNames.cs
@@ -3,12 +3,15 @@ using Assets.Utils.Extensions;
 
 namespace Assets.Helpers
 {
+
     public static class CharactersNames
     {
+        public static string ProtagonistName { get; set; }
+
         public static string Get(Character character)
         {
             if (character == Character.Protagonist)
-                return "PROTAGONISTA";
+                return ProtagonistName;
 
             return character.GetStringValue().ToUpper();
         }
--- Equipe5/GameProject/TheUpsideDown/Assets/Scripts/MainMenu.cs
+++ Equipe5/GameProject/TheUpsideDown/Assets/Scripts/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using System;
+using Assets.Helpers;
 public class MainMenu : MonoBehaviour
 {
+    
     public void StartGame()
     {
+        CharactersNames.ProtagonistName = GameObject.Find("UserNameField").GetComponent<InputField>().text;
+        PlayerPrefs.SetString("UserName", CharactersNames.ProtagonistName);
+        PlayerPrefs.Save();
         Loader.Load(Assets.Enums.GameScene.FirstScenario);
     }
 
