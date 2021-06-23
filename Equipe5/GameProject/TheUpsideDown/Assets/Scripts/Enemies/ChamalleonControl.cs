55bd47335f0cff1b499eb781c6692c97567b2bf4
Alan Jantz <alan_f_jantz@hotmail.com>
2021-06-23 15:01:03 -0300

    Cenário nível 3

--- Equipe5/GameProject/TheUpsideDown/Assets/Scripts/Enemies/ChamalleonControl.cs
+++
@@ -1,4 +0,0 @@
-public class ChamalleonControl : BaseEnemy
-{
-    protected override string TagName => "Chamalleon";
-}
---
+++ Equipe5/GameProject/TheUpsideDown/Assets/Scripts/Enemies/ChameleonControl.cs
@@ -0,0 +1,4 @@
+public class ChameleonControl : BaseEnemy
+{
+    protected override string TagName => "Chameleon";
+}
